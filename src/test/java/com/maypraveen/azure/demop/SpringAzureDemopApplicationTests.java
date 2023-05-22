038e115eabd63c2eb65f7ee31239b0ddde510fa9
Praveen512P <gpj.praveen512@gmail.com>
2023-05-22 14:08:46 +0530

    first commit

---
+++ src/test/java/com/maypraveen/azure/demop/SpringAzureDemopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maypraveen.azure.demop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAzureDemopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
